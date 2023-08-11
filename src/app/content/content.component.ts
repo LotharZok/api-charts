eb38a63f129ab93b7538fc609f6bf1d076b2648f
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-08-11 09:40:08 +0200

    added some documentation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -15,15 +15,21 @@ export class ContentComponent {
     chartData: number[] = [];
     chartLow: number[] = [];
     chartHigh: number[] = [];
-    chartLowHigh: number[][] = [];
+    // chartLowHigh: number[][] = [];
     curDate: string = new Date().toDateString();
 
+    /**
+     * Constructor: Starts loading of the rates
+     */
     constructor() {
         // console.log(API_KEY);
         this.loadRate();
     }
 
 
+    /**
+     * Loads the current Bitcoin rate and starts loading of historical data.
+     */
     async loadRate() {
         let url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=EUR&apikey=' + API_KEY;
         let response = await fetch(url);
@@ -34,40 +40,40 @@ export class ContentComponent {
     }
 
 
+    /**
+     * Loads the historical Bitcoins rates and calls the function to draw the chart.
+     */
     async loadMonthlyRates() {
         let url = 'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=BTC&market=EUR&apikey=' + API_KEY;
         let response = await fetch(url);
         let responseAsJson = await response.json();
-        console.log('Exchange rate: ', responseAsJson['Time Series (Digital Currency Monthly)']);
+        // console.log('Exchange rate: ', responseAsJson['Time Series (Digital Currency Monthly)']);
         let keysArray = Object.keys(responseAsJson['Time Series (Digital Currency Monthly)']);
 
         for (let i = keysArray.length - 1; i >= 0; i--) {
 
             const elem = keysArray[i];
             const rateEachMonth = responseAsJson['Time Series (Digital Currency Monthly)'][elem];
-            // console.log('Rate ' + elem + ': ', rateEachMonth);
             const closeRate = rateEachMonth['4a. close (EUR)'];
-            // console.log('Closing rate ' + elem + ': ', closeRate);
-            let lowHigh:number[] = [rateEachMonth['2a. high (EUR)'], rateEachMonth['3a. low (EUR)']];
-            console.log(lowHigh);
+            // Test-Variable for a floating bars chart (doesn't work)
+            // let lowHigh:number[] = [rateEachMonth['2a. high (EUR)'], rateEachMonth['3a. low (EUR)']];
     
             this.chartLabels.push(elem);
             this.chartData.push(closeRate);
             this.chartHigh.push(rateEachMonth['2a. high (EUR)']);
             this.chartLow.push(rateEachMonth['3a. low (EUR)']);
-            // console.log(this.chartLabels);
-            // console.log(this.chartData);
-            // this.chartLowHigh = [this.chartLow, this.chartHigh];
-            this.chartLowHigh.push(lowHigh);
+            // Test-Variable for a floating bars chart (doesn't work)
+            // this.chartLowHigh.push(lowHigh);
         }
-        console.log(this.chartLowHigh);
         this.drawChart()
     }
 
 
+    /**
+     * Draws the chart using techniques from chart.js
+     */
     drawChart() {
         this.chart = new Chart('canvas', {
-            // type: 'line',
             data: {
                 labels: this.chartLabels,
                 datasets: [
@@ -100,6 +106,8 @@ export class ContentComponent {
             },
         });
 
+        // Floating chart doesn't work, therefor the following test part is deactivated
+
         // this.chart = new Chart('canvas', {
         //     type: 'bar',
         //     data: this.chartLowHigh,
@@ -118,4 +126,3 @@ export class ContentComponent {
         // })
     }
 }
-
